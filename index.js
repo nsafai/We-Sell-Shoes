a2c7e6c89507082df6b4c4764cfc45dd78019a39
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-03 21:16:37 -0700

    added popup and fixed navbar, with hacky css rule on li

--- index.js
+++ index.js
@@ -136,3 +136,35 @@ for (let i = 0; i < acc.length; i++) {
     }
   });
 }
+
+/////////popup//////////////
+
+//declare default vars
+const popUp = document.getElementById('pop-up');
+const popUpLayer = document.getElementById('pop-up-layer');
+const closeModal = document.getElementById('close-pop-up');
+
+//nav-bar button
+const subBtn = document.getElementById('sub-btn');
+
+//navbar button click makes the popup appear
+subBtn.addEventListener("click", () => {
+  popUpLayer.style.display = 'flex';
+})
+
+//close model click makes the popup disappear
+closeModal.addEventListener("click", function() {
+  popUpLayer.style.display = 'none';
+})
+
+let popUpSeconds = 40;
+
+//trigger popup after seconds variable
+let popUpFun = window.setInterval(function() {
+  popUpLayer.style.display = 'flex';
+
+  window.clearInterval(popUpFun);
+
+}, popUpSeconds * 10000); // 10 seconds till automatic pop up
+
+///////////////////
